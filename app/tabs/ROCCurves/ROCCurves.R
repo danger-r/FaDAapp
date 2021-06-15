b41b146254461271bf5e7960f63969732b552d90
danger-r <37658659+danger-r@users.noreply.github.com>
2021-06-15 18:01:55 +0200

    Add files via upload

--- app/tabs/ROCCurves/ROCCurves.R
+++ app/tabs/ROCCurves/ROCCurves.R
@@ -6,11 +6,9 @@ ROCCurvesUI <- function(){
     uiOutput(outputId ="group1"),
     uiOutput(outputId ="group2"),
     plotOutput(outputId ="ROCPlot", width = "50%")  %>% withSpinner(color="#0dc5c1"),
-
-    downloadButton('dROCPlotTiff', label="Download as .Tiff"),
-    downloadButton('dROCPlotSvg', label="Download as .SVG"),
+    downloadButton(outputId ='dROCPlot', label="Download"),
     tags$br(),
-    tags$h3("ROC analysis:",style = "color: steelblue;"),
+    tags$h3("AUC table:",style = "color: steelblue;"),
     DTOutput(outputId ="AUC_table")  %>% withSpinner(color="#0dc5c1"),
     tags$br()
   )
@@ -20,7 +18,7 @@ ROCCurvesUI <- function(){
 ROCPlot <- function(input,reacUsedTable,reacColorFunction){
   ROCPlot <- function(){
     validate(
-     need( !is.null(reacUsedTable()), " " ),
+     need( !is.null(reacUsedTable()), "Please select a properly formatted data set" ),
      need( input$group1 != input$group2, "You must select differents groups" )
     )
     funROCPlot(reacUsedTable(),input$SelectColor,input$group1,input$group2,input$parameter1)
@@ -31,8 +29,8 @@ ROCPlot <- function(input,reacUsedTable,reacColorFunction){
 AUCTable <- function(input,reacUsedTable){
   AUC_table <- reactive({
     validate(
-      need( !is.null(reacUsedTable()), "  " ),
-      need( input$group1 != input$group2, " " )
+      need( !is.null(reacUsedTable()), "Please select a properly formatted data set" ),
+      need( input$group1 != input$group2, "You must select differents groups" )
     )
 
     funAUCTable(reacUsedTable(),input$group1,input$group2)
@@ -55,10 +53,11 @@ funROCPlot <- function(used_Table,infoSelectColor,infoGroup1,infoGroup2,infoPara
     pos <- 0.4 - 0.05*(which(infoParameter1 == parameter)-1) #Calculate the position of the AUC print in the graph
     gcol <- colors[which(colnames(used_Table[,-1]) == parameter)] #Associate the color of the AUC print with the corresponding courb
     plot(my_roc, xlim=c(1,0), print.auc=TRUE, print.auc.pattern="AUC : %.3f", print.auc.col=gcol,
-         col=gcol, print.auc.y=pos, print.auc.x=0.5, lwd=4, lty=1,add = TRUE)
+         percent=F, col=gcol, print.auc.y=pos, print.auc.x=0.5, lwd=4, lty=1,add = TRUE)
     par(new=TRUE) #Put multiple graph in single plot
   })
 
+  # return(gROC)
 }
 
 funAUCTable <- function(used_Table,infoGroup1,infoGroup2){
@@ -70,8 +69,7 @@ funAUCTable <- function(used_Table,infoGroup1,infoGroup2){
     roc( as.numeric(as.factor(group_df3)), as.numeric(x))$auc )
   Valroc[c(2:6),] <- apply(df4, 2, function(x){ #Get the others values
      rocc <- roc( as.numeric(as.factor(group_df3)), as.numeric(x))
-     val <- coords(rocc,"best",ret = c("threshold","specificity","sensitivity","npv","ppv"),
-                   transpose = TRUE)
+     val <- coords(rocc,"best",ret = c("threshold","specificity","sensitivity","npv","ppv"))
      if(class(val) == "matrix"){
         return(val[,1])
      }else{
@@ -87,7 +85,7 @@ funAUCTable <- function(used_Table,infoGroup1,infoGroup2){
 ROCCurvesOutput <- function(output,reacCalcTable,reacROCPlot,reacAUCTable,reacUsedGroups,reacNameTable){
   output$AUC_table <- renderDT({     datatable(reacAUCTable(),
                                                extensions="Buttons",
-                                               options = list(pageLength = 20, searching = FALSE, dom = 'tB',
+                                               options = list(pageLength = 20, searching = FALSE, dom = 'Bt',
                                                               buttons = list( 'copy',
                                                                                  list(extend='csv',
                                                                                       filename = paste(reacNameTable(),"_AUCTable",sep="")),
@@ -101,33 +99,27 @@ ROCCurvesOutput <- function(output,reacCalcTable,reacROCPlot,reacAUCTable,reacUs
   output$ROCPlot <- renderPlot({    reacROCPlot()    })
 
   output$parameter1 <- renderUI({  df <- reacCalcTable()
-    selectInput("parameter1","Parameter(s) to test:", colnames(df), selected = colnames(df)[1], multiple = TRUE)  })
+    selectInput("parameter1","Parameter to test:", colnames(df), selected = colnames(df)[1], multiple = TRUE)  })
 
   output$group1 <- renderUI({
+
                                group <- reacUsedGroups()
-                               selectInput("group1","First group:", levels(as.factor(group)),
+                               selectInput("group1","First group to test:", levels(as.factor(group)),
                                             selected = levels(as.factor(group))[1] )  })
 
   output$group2 <- renderUI({
                                group <- reacUsedGroups()
-                               selectInput("group2","Second group:", levels(as.factor(group)),
+                               selectInput("group2","Second group to test:", levels(as.factor(group)),
                                             selected = levels(as.factor(group))[2] )  })
 
 }
 
 ## Download ####
 ROCDownload <- function(input,output,reacROCPlot){
-  output$dROCPlotTiff = downloadHandler(filename =
-                                              reactive(paste(input$file1$name,"_ROCCurve.tiff",sep = "")),
-                                            content = function(file, compression = "lzw", res = 600) {
-                                              tiff(file)
-                                              print( reacROCPlot() )
-                                              dev.off()                      })
-  output$dROCPlotSvg = downloadHandler(filename =
-                                             reactive(paste(input$file1$name,"_ROCCurve.svg",sep = "")),
-                                           content = function(file, compression = "lzw", res = 600) {
-                                             svglite(file)
-                                             print( reacROCPlot() )
-                                             dev.off()                      })
+  output$dROCPlot = downloadHandler(filename =
+                                      reactive(paste(input$file1$name,"_ROCCurves.tiff",sep = "")), #set the filename of the downloaded file
+                                    content = function(file, compression = "lzw", res = 600) {
+                                      tiff(file)
+                                      print( reacROCPlot() )
+                                      dev.off()                      }, contentType = "image/png")
 }
-####End
