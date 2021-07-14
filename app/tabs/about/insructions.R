3c18574e0e15179da1fbe507ee578cd5eab3e9e3
danger-r <37658659+danger-r@users.noreply.github.com>
2021-07-14 23:58:30 +0200

    Update insructions.R

--- app/tabs/about/insructions.R
+++ app/tabs/about/insructions.R
@@ -1,7 +1,7 @@
 ## UI ####
 introductionUI <- function(){
   tagList(
-    htmlOutput("Textintro")
+    htmlOutput("Textintro")   ##Display text
   )
 }
 ## Serveur Functions ####
@@ -13,11 +13,15 @@ introduction <- function(){
       tags$br(),
       tags$h3("FaDA Data Analysis Tools", style =  "color: #337ab7;"),
       tags$br(),
-    tags$h4("Prepare and Upload Your Dataset", style="color: #33b7b2;"),
+      a("Various statistical options are supported with FaDA,
+        however, conclusions should be drawn only with adequate methods and recommendations from a statistician should be seek for if needed."),
+
+      tags$br(),
+      tags$h4("Prepare and Upload Your Dataset", style="color: #33b7b2;"),
     "To allow a flexible use of the application with a minimum preparation time, samples identification can be in columns or rows. FaDA input requires:",
         tags$li("First column or row of sample identifiers", style="list-style-type: square;"),
         tags$li("All identifiers should have unique names", style="list-style-type: square;"),
-        tags$li("Second row or column must be labeled as ", a("Group"), style="list-style-type: square;"),
+        tags$li("Second row or column must be labeled as ", tags$b(a("Group")), style="list-style-type: square;"),
         tags$li("Points or comma could be used as decimal separators", style="list-style-type: square;"),
         tags$li("Table of data must be a text format: tabular-delimited '.txt' or '.csv' file", style="list-style-type: square;"),
     tags$br(),
@@ -32,15 +36,19 @@ introduction <- function(){
     tags$br(),
 
     tags$h4("Statistical Summary", style="color: #33b7b2;"),
-    "FaDA provides comprehensive statistical analysis options on the sidebar, with an option to transform data to log2 or log10 values. Statistical analysis output can be downloaded in CSV or Excel files.",
+    "FaDA provides comprehensive statistical analysis options on the sidebar, and an option to transform data to log2 or log10 values. Statistical analysis output can be downloaded in CSV or Excel files.",
 
 
    tags$h4("Selecting suitable Statistical Analysis Method", style="color: #33b7b2;"),
+   "Parametric tests are commonly used but require a normal sampling distribution. If the sample size is large enough, approximately normally distributed samples can be analysed using parametric tests (central limit theorem). Nonparametric tests do not make assumption about the underlying distribution of variables. Such tests should be used with small sample size, non-normal distributed data, ordinal data and ranked data. They are thus more robust but also frequently less powerful.",
+   tags$br(),
    "The p-value of the Shapiro-Wilk normality test indicates whether the distribution of the dataset differs from Gaussian distribution guiding the users toward parametric or nonparametric test.",
-    tags$br(),
-    tags$li( tags$u("Parametric statistical analysis:"), "descriptive table provides the mean and standard deviation of the uploaded data. Group comparisons are performed using parametric t.test or ANOVA test with Tukey's 'Honest Significant Difference' method for multiple groups comparisons.", style="list-style-type: square;"),
-  tags$li(tags$u("Non-parametric Analysis:"),"this setting is set to provide median and interquartile range (IQR) per groups with group comparisons performed using Mann-Whitney or the Kruskal Wallis test with Dunn's test for multiple comparisons.", style="list-style-type: square;"),
-    tags$li(tags$u("Multiple Test Correction:"),"p-values corrections are performed using Bonferroni or Benjamin & Hochberg (BH) methods to correct for positive error rates due to multiple testing.", style="list-style-type: square;"),
+   tags$br(),
+   tags$br(),
+    tags$li( tags$u("Parametric statistical analysis:"), "descriptive table provides the mean and standard deviation of the uploaded data. Group comparisons are performed using parametric t.test or ANOVA test with Tukey's 'Honest Significant Difference' method for multiple groups comparisons. The Welch's t.test is proposed in case of unequal variance.", style="list-style-type: square;"),
+  tags$li(tags$u("Non-parametric analysis:"),"using this setting provides median and interquartile range (IQR) per groups with group comparisons performed using Mann-Whitney or the Kruskal Wallis test with Dunn's test for multiple comparisons.", style="list-style-type: square;"),
+    tags$li(tags$u("Multiple Test Correction:"),"p-values can be adjusted to account for multiple comparisons in order to avoid false positives. FaDA allows to perform corrections using the Bonferroni method that controls the family-wise error rate or the Benjamin & Hochberg (BH) method that corrects the the false discovery rate (FDR), which is the proportion of false discoveries among the rejected hypotheses.
+While the Bonferroni method is useful with a fairly small number of comparisons, it may be too conservative and may lead to a high rate of false negatives. Both methods assume that the individual tests are independents (not correlated).", style="list-style-type: square;"),
     tags$br(),
 
     tags$h4("Data Visualization Options", style="color: #33b7b2;"),
@@ -84,8 +92,8 @@ introduction <- function(){
 
   tags$br(),
   "Uploaded data and analysis results will not be saved on our servers. However, the application is provided", '" as is "', "without warranty of any kind, and thus, you must refrain fom uploading any confidential data. You may instead download the code and run the app locally on your private computer and network. We are not responsible for the confidentiality, availability, security, loss, misuse or misappropriation of any data you submit to this application.", style = "color: black;",
-  tags$h4("Copyright 2020 Center of Research in Transplantation and Immunology
-Version 2020.02.03", align="center"),
+  tags$h4("Copyright 2021 Center of Research in Transplantation and Immunology
+Version 2021.07.12", align="center"),
 
 
 
@@ -111,3 +119,4 @@ introductionOutput <- function(output,Textintro){
   output$Textintro <- renderUI({  Textintro() })
 }
 
+####END####
